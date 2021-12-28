ad30aa5ece19a1932748cddb2343b25b9d43b6d0
VinayMalviya2021 <vinay.malviya@paytmmloyal.com>
2021-12-29 00:05:30 +0530

    Login detail

---
+++ HPCL-Web/Helper/WebApiUrl.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Helper
+{
+    public class WebApiUrl
+    {
+
+        #region Account
+        
+        public string generatetoken = "dtp/api/dtplus/generatetoken";
+        public string getuserlogin = "dtp/api/dtplus/login/get_user_login";
+
+        #endregion
+
+    }
+}
