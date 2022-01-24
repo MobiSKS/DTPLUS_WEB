c66bd15c145f2cde5c216dac9781c78b626a5405
Sujit Mobiquest <sujit@mobiquest.com>
2022-01-24 12:52:05 +0530

    commit final code

---
+++ HPCL-Web/Controllers/OfficerController.cs
@@ -0,0 +1,38 @@
+﻿using HPCL_Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Controllers
+{
+    public class OfficerController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> EditOfficer()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditOfficer(OfficerModel ofcrMdl)
+        {
+
+            return View();
+        }
+
+        public async Task<IActionResult> EditLocation()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> EditLocation(OfficerLocationModel OfcrLocation)
+        {
+            return View();
+        }
+    }
+}
---
+++ HPCL-Web/Models/OfficerModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Models
+{
+    public class OfficerModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string OfficerType { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Address3 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string District { get; set; }
+        public Int32 Pin { get; set; }
+        public Int32 Mobile { get; set; }
+        public Int32 Phone { get; set; }
+        public string Email { get; set; }
+        public string Fax { get; set; }
+        public virtual IEnumerable<OfficerTypeModel> OfficerTypeMdl { get; set; }
+    }
+
+    public class OfficerTypeModel
+    {
+        public int OfficerID { get; set; }
+        public string OfficerTypeName { get; set; }
+    }
+}
