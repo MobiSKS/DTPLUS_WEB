1ca9d9c807067cefce4c40f8802c37ef66915003
Gaurav Verma <gaurav@paytmmloyal.com>
2022-01-27 12:03:03 +0530

    hq Changes api

--- HPCL-Web/Helper/WebApiUrl.cs
+++ HPCL-Web/Helper/WebApiUrl.cs
@@ -13,7 +13,7 @@
         public static string getOfficerType = "api/dtplus/officer/get_officer_type";
         public static string getLocationZone = "api/dtplus/settings/get_zone";
         public static string getLocationRegion = "api/dtplus/settings/get_region";
-        public static string getLocationHq = "/api/dtplus/hq/get_hq";
+        public static string getLocationHq = "api/dtplus/hq/get_hq";
         public static string getState = "api/dtplus/settings/get_state";
         public static string getDistrict = "api/dtplus/settings/get_district";
         public static string validateUserName = "api/dtplus/officer/check_username";
