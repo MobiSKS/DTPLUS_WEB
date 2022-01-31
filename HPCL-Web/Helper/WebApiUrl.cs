366c5dd884e0a1f495a73c38557a2d96ae53d841
Gaurav Verma <gaurav@paytmmloyal.com>
2022-01-31 13:15:41 +0530

    Changed WebAPIUrl

--- HPCL-Web/Helper/WebApiUrl.cs
+++ HPCL-Web/Helper/WebApiUrl.cs
@@ -10,20 +10,20 @@
         #endregion
         #region Officer
 
-        public static string getOfficerType = "/dtpwebapi/api/dtplus/officer/get_officer_type";
-        public static string getLocationZone = "/dtpwebapi/api/dtplus/settings/get_zone";
-        public static string getLocationRegion = "/dtpwebapi/api/dtplus/settings/get_region";
-        public static string getLocationHq = "/dtpwebapi/api/dtplus/hq/get_hq";
-        public static string getState = "/dtpwebapi/api/dtplus/settings/get_state";
-        public static string getDistrict = "/dtpwebapi/api/dtplus/settings/get_district";
-        public static string validateUserName = "/dtpwebapi/api/dtplus/officer/check_username";
-        public static string insertOfficer = "/dtpwebapi/api/dtplus/officer/insert_officer";
-        public static string updateOfficer = "/dtpwebapi/api/dtplus/officer/update_officer";
-        public static string getOfficerDetails = "/dtpwebapi/api/dtplus/officer/get_officer_detail";
-        public static string deleteOfficer = "/dtpwebapi/api/dtplus/officer/delete_officer";
-        public static string bindOfficer = "/dtpwebapi/api/dtplus/officer/bind_officer";
-        public static string zonalOffice = "/dtpwebapi/api/dtplus/zonaloffice/get_zonal_office";
-        public static string regionalOffice = "/dtpwebapi/api/dtplus/regionaloffice/get_regional_office";
+        public static string getOfficerType = "api/dtplus/officer/get_officer_type";
+        public static string getLocationZone = "api/dtplus/settings/get_zone";
+        public static string getLocationRegion = "api/dtplus/settings/get_region";
+        public static string getLocationHq = "api/dtplus/hq/get_hq";
+        public static string getState = "api/dtplus/settings/get_state";
+        public static string getDistrict = "api/dtplus/settings/get_district";
+        public static string validateUserName = "api/dtplus/officer/check_username";
+        public static string insertOfficer = "api/dtplus/officer/insert_officer";
+        public static string updateOfficer = "api/dtplus/officer/update_officer";
+        public static string getOfficerDetails = "api/dtplus/officer/get_officer_detail";
+        public static string deleteOfficer = "api/dtplus/officer/delete_officer";
+        public static string bindOfficer = "api/dtplus/officer/bind_officer";
+        public static string zonalOffice = "api/dtplus/zonaloffice/get_zonal_office";
+        public static string regionalOffice = "api/dtplus/regionaloffice/get_regional_office";
         public static string createHeadOffice= "/dtpwebapi/api/dtplus/hq/insert_hq";
         public static string updateHeadOffice = "/dtpwebapi/api/dtplus/hq/update_hq";
 
