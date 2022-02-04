7083ff4e87bf3ca4399e2a5d0e68152663e02e99
Hemalatha <Hemalatha@DESKTOP-0JEHRLO>
2022-02-04 10:56:46 +0530

    added customer APi urls

--- HPCL-Web/Helper/WebApiUrl.cs
+++ HPCL-Web/Helper/WebApiUrl.cs
@@ -28,13 +28,7 @@
         public static string createHeadOffice= "/dtpwebapi/api/dtplus/hq/insert_hq";
         public static string updateHeadOffice = "/dtpwebapi/api/dtplus/hq/update_hq";
 
-        #endregion
-
-        #region Customer
-
-        public static string getCustomerType= "/dtpwebapi/api/dtplus/customer/get_customer_type";
-
-        #endregion
+        #endregion       
 
         #region Cards
 
