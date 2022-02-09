5e8b10bb87e03398f54fa00eea82cd563543ba41
DESKTOP-RCN6RRA\Ravi <ravi.samudrala@girmiti.com>
2022-02-09 16:42:57 +0530

    Customer Save

--- HPCL-Web/Helper/WebApiUrl.cs
+++ HPCL-Web/Helper/WebApiUrl.cs
@@ -16,25 +16,25 @@
         public static string getLocationHq = "api/dtplus/hq/get_hq";
         public static string getState = "api/dtplus/settings/get_state";
         public static string getDistrict = "api/dtplus/settings/get_district";
-        public static string getSalesArea = "api/dtplus/settings/get_sales_area";
         public static string validateUserName = "api/dtplus/officer/check_username";
         public static string insertOfficer = "api/dtplus/officer/insert_officer";
+        public static string getSalesArea = "api/dtplus/settings/get_sales_area";
         public static string updateOfficer = "api/dtplus/officer/update_officer";
         public static string getOfficerDetails = "api/dtplus/officer/get_officer_detail";
         public static string deleteOfficer = "api/dtplus/officer/delete_officer";
         public static string bindOfficer = "api/dtplus/officer/bind_officer";
         public static string zonalOffice = "api/dtplus/zonaloffice/get_zonal_office";
         public static string regionalOffice = "api/dtplus/regionaloffice/get_regional_office";
-        public static string createHeadOffice= "/dtpwebapi/api/dtplus/hq/insert_hq";
+        public static string createHeadOffice = "/dtpwebapi/api/dtplus/hq/insert_hq";
         public static string updateHeadOffice = "/dtpwebapi/api/dtplus/hq/update_hq";
 
         #endregion       
 
         #region Cards
 
-        public static string GetStatusTypeUrl= "/dtpwebapi/api/dtplus/settings/get_entity_status_type";
+        public static string GetStatusTypeUrl = "/dtpwebapi/api/dtplus/settings/get_entity_status_type";
         public static string SearchCardUrl = "/dtpwebapi/api/dtplus/card/search_manage_card";
-        public static string GetCardDetailsUrl= "/dtpwebapi/api/dtplus/card/get_card_limit_features";
+        public static string GetCardDetailsUrl = "/dtpwebapi/api/dtplus/card/get_card_limit_features";
         public static string UpdateMobileUrl = "/dtpwebapi/api/dtplus/card/update_mobile_in_card";
         public static string UpdateServiceUrl = "/dtpwebapi/api/dtplus/card/update_service_on_card";
         public static string GetAllCardStatusUrl = "/dtpwebapi/api/dtplus/card/get_all_card_with_status";
@@ -70,26 +70,8 @@
         public static string uplaodCustomerKyc = "/dtpwebapi/api/dtplus/customer/upload_customer_kyc";
 
 
-
         #endregion
 
-
-
-        #region customer
-
-        public static string getCustomerType = "/dtpwebapi/api/dtplus/customer/get_customer_type";
-        public static string getZonalOffice = "/dtpwebapi/api/dtplus/zonaloffice/get_zonal_office";
-        public static string getRegionalOffice = "/dtpwebapi/api/dtplus/regionaloffice/get_regional_office";
-        public static string getCustomerSubType = "/dtpwebapi/api/dtplus/customer/get_customer_sub_type";
-        public static string getTbentityName = "/dtpwebapi/api/dtplus/customer/get_tbentity_name";
-        public static string getTypeOfFleet = "/dtpwebapi/api/dtplus/customer/get_type_of_fleet";
-        public static string getSecretQuestion = "/dtpwebapi/api/dtplus/customer/get_secret_question";
-        public static string insertCustomer = "/dtpwebapi/api/dtplus/customer/insert_customer";
-        public static string getVehicleTpe = "/dtpwebapi/api/dtplus/customer/get_vehicle_type";
-        public static string uplaodCustomerKyc = "/dtpwebapi/api/dtplus/customer/upload_customer_kyc";
-
-
-
-        #endregion
+       
     }
 }
