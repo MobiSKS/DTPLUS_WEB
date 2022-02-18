e29ea495aadcc3b36198baae1104b6ffacde41ce
Prasanta Pande <prasanta.pande1997@gmail.com>
2022-02-18 10:01:32 +0530

    ViewCard

--- HPCL-Web/Helper/WebApiUrl.cs
+++ HPCL-Web/Helper/WebApiUrl.cs
@@ -50,7 +50,7 @@
         public static string SearchCcmsIndividualCardLimitUrl = "/dtpwebapi/api/dtplus/card/get_ccms_limit";
         public static string UpdateCcmsIndividualCardLimitUrl = "/dtpwebapi/api/dtplus/card/update_ccms_limits";
         public static string ViewCardLimitsUrl = "/dtpwebapi/api/dtplus/card/view_card_limits";
-        public static string MobileAddOrEdit = "/dtpwebapi/api/dtplus/card/update_mobile_and_fastag_no_in_card";
+        public static string MobileAddOrEditUrl = "/dtpwebapi/api/dtplus/card/update_mobile_and_fastag_no_in_card";
         public static string getValidateNewCardLists = "api/dtplus/card/bind_pending_customer_for_card_approval";
         public static string getCardDetailsForCardApproval = "api/dtplus/card/get_card_detail_for_card_approval";
         public static string approveRejectCard = "api/dtplus/card/approve_reject_card";
