047b22508e56015d3c79fc52da3922d5f9f6954d
Gaurav Verma <gaurav@paytmmloyal.com>
2022-02-20 20:20:40 +0530

    Commit Changes for officer, merchant and some common files

---
+++ HPCL-Web/Controllers/TerminalManagementController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Controllers
+{
+    public class TerminalManagementController : Controller
+    {
+        public IActionResult ManageTerminal()
+        {
+            return View();
+        }
+        public IActionResult RegenerateIac()
+        {
+            return View();
+        }
+        public IActionResult TerminalInstallationRequest()
+        {
+            return View();
+        }
+        public IActionResult UnblockTerminal()
+        {
+            return View();
+        }
+    }
+}
---
+++ HPCL-Web/Models/Officer/OfficerDetailsModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Models.Officer
+{
+    public class OfficerDetailsModel
+    {
+        public OfficerDetailsModel()
+        {
+            OfficerZones = new List<OfficerZoneModel>();
+            OfficerZones.Add(new OfficerZoneModel { 
+            ZonalOfficeID = 0,
+            ZonalOfficeName = "--ALL--"
+            });
+
+            OfficerStates = new List<OfficerStateModel>();
+            OfficerStates.Add(new OfficerStateModel
+            {
+                CountryID = 0,
+                StateID = 0,
+                StateName = "--ALL--"
+            });
+        }
+        public string ZonalOfcID { get; set; }
+        public string RegionalOfcID { get; set; }
+        public string StateID { get; set; }
+        public string DistrictID { get; set; }
+        public virtual List<OfficerZoneModel> OfficerZones { get; set; }
+        public virtual List<OfficerRegionModel> OfficerRegions { get; set; }
+        public virtual List<OfficerStateModel> OfficerStates { get; set; }
+        public virtual List<OfficerDistrictModel> OfficerDistricts { get; set; }
+    }
+}
