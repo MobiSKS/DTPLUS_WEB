ed337a85852e2b9940da22e2029d9cc6a6a180f8
Gaurav Verma <gaurav@paytmmloyal.com>
2022-02-21 18:31:35 +0530

    Commiting Changes for 1. Officer Details API Integration 2. Additional Terminal Pages

--- HPCL-Web/Controllers/TerminalManagementController.cs
+++ HPCL-Web/Controllers/TerminalManagementController.cs
@@ -24,5 +24,45 @@ namespace HPCL_Web.Controllers
         {
             return View();
         }
+
+        public IActionResult TerminalInstallationRequestClose()
+        {
+            return View();
+        }
+
+        public IActionResult ViewTerminalDeinstallationRequestStatus()
+        {
+            return View();
+        }
+
+        public IActionResult ViewTerminalInstallationRequestStatus()
+        {
+            return View();
+        }
+
+        public IActionResult ViewTerminalMerchantMappingStatus()
+        {
+            return View();
+        }
+
+        public IActionResult ManageAutomationIntegrationStatus()
+        {
+            return View();
+        }
+
+        public IActionResult ProblematicDeInstalledToDeInstalled()
+        {
+            return View();
+        }
+
+        public IActionResult TerminalDeInstallationRequest()
+        {
+            return View();
+        }
+
+        public IActionResult TerminalDeInstallationRequestClose()
+        {
+            return View();
+        }
     }
 }
--- HPCL-Web/Models/Officer/OfficerModel.cs
+++ HPCL-Web/Models/Officer/OfficerModel.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace HPCL_Web.Models.Officer
 {
-    public class OfficerModel
-    {
+    public class OfficerModel    {
         
         public OfficerModel()
         {
