7cd4c9474645003da0f984f14e13ffc4901e3597
Gaurav Verma <gaurav@paytmmloyal.com>
2022-02-24 19:50:15 +0530

    Commit Changes for Officer Views

--- HPCL.Common/Resources/Label.cs
+++ HPCL.Common/Resources/Label.cs
@@ -20,6 +20,9 @@
         public const string OfficerType = "Officer Type";
         public const string UserName = "User Name";
         public const string Location = "Location";
+        public const string IsNewUser = "Is New User";
+        public const string ZO = "ZO";
+        public const string RO = "RO";
 
         #endregion
 
@@ -54,6 +57,12 @@
         public const string Reset = "RESET";
         public const string UpdateMobile = "UPDATE MOBILE";
         public const string Back = "BACK";
+        public const string Add = "ADD";
+        public const string Cancel = "CANCEL";
+        public const string Save = "SAVE";
+        public const string AddOfficer = "ADD OFFICER";
+        public const string AddLocation = "ADD LOCATION";
+        public const string ExportToExcel = "Export To Excel";
 
         #endregion
 
@@ -71,11 +80,14 @@
         public const string Address3 = "Address 3";
         public const string City = "City";
         public const string State = "State";
+        public const string StateName = "State Name";
         public const string District = "District";
+        public const string DistrictName = "District Name";
         public const string FirstName = "First Name";
         public const string LastName = "Last Name";
         public const string Name = "Name";
         public const string ZonalOfc = "Zonal Office";
+        public const string ZoneOfc = "Zone Office";
         public const string RegionalOfc = "Regional Office";
         public const string SalesArea = "Sales Area";
         public const string AccountHolderName = "Account Holder Name";
@@ -83,6 +95,12 @@
         public const string BranchLocation = "Branch Location";
         public const string DtpCautionLimitInRs = "DTP Caution Limit in RS";
         public const string HpPayCautionLimitInRs = "HPPay Caution Limit in RS";
+        public const string SerialNumber = "S No.";
+        public const string Regions = "Regions";
+        public const string MarketingOfficerName = "Marketing Officer Name";
+        public const string MarketingOfficerEmail = "Marketing Officer Email";
+        public const string ZonalOfficer = "Zonal Officer";
+        public const string ZonalOfficerEmail = "Zonal Officer Email";
 
         #endregion
     }
