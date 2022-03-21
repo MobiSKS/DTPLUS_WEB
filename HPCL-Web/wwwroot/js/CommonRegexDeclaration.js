336e09e98f0a6e739bb5a7627576aef0e0665d19
DESKTOP-RCN6RRA\Ravi <ravi.samudrala@girmiti.com>
2022-03-21 14:44:13 +0530

    Jira issues corrected

--- HPCL-Web/wwwroot/js/CommonRegexDeclaration.js
+++ HPCL-Web/wwwroot/js/CommonRegexDeclaration.js
@@ -9,3 +9,4 @@ var lpgCngSale = /^(?:[1-9][0-9]{0,4}(d{1,2})?|100000|100000.00)$/;
 var erpCodeCheck = /^(?=(1))[0-9]{8}$/;
 var nameCheck = /^[A-Za-z]{2,40}$/;
 var noOfTerminalsCheck = /^[1-2]{1}$/;
+var nameWithSpaceCheck = /[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/;
