f532e25008879c0f0205df0245f60a582fafedfd
DESKTOP-RCN6RRA\Ravi <ravi.samudrala@girmiti.com>
2022-03-23 00:03:56 +0530

    ERP Reload/Sale/Earning Details

---
+++ HPCL.Common/Models/ViewModel/MerchantFinancials/MerchantERPReloadSaleEarningModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HPCL.Common.Models.ViewModel.MerchantFinancials
+{
+    public class MerchantERPReloadSaleEarningModel
+    {
+        public string MerchantId { get; set; }
+        public string TerminalOrMerchant { get; set; }
+        public string FromDate { get; set; }
+        public string ToDate { get; set; }
+        public List<MerchantERPReloadSaleEarningDetails> SaleEarningDetails { get; set; }
+        public string Message { get; set; }
+        public MerchantERPReloadSaleEarningModel()
+        {
+            SaleEarningDetails = new List<MerchantERPReloadSaleEarningDetails>();
+        }
+    }
+
+    public class MerchantERPReloadSaleEarningDetails
+    {
+        public string TerminalID { get; set; }
+        public string BatchID { get; set; }
+        public string SettlementDate { get; set; }
+        public string Sale { get; set; }
+        public string Reload { get; set; }
+        public string Earning { get; set; }
+    }
+}
