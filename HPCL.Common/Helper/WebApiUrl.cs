07bfb9a1f43dcb4508800444a464e58de7c4ac48
Biplab Behera <biplab@paytmmloyal.com>
2022-03-22 19:51:09 +0530

    fixes

--- HPCL.Common/Helper/WebApiUrl.cs
+++ HPCL.Common/Helper/WebApiUrl.cs
@@ -14,7 +14,7 @@
         //public static string getLocationZone = "api/dtplus/settings/get_zone";
         //public static string getLocationRegion = "api/dtplus/settings/get_region";
         public static string getLocationHq = "api/dtplus/hq/get_hq";
-        public static string getState = "​api​/dtplus​/state​/get_state";
+        public static string getState = "api/dtplus/state/get_state";
         public static string getDistrict = "api/dtplus/district/get_district";
         public static string validateUserName = "api/dtplus/officer/check_username";
         public static string insertOfficer = "api/dtplus/officer/insert_officer";
