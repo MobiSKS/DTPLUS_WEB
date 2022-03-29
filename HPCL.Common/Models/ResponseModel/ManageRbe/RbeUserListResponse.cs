4496ea87dc0dd5139956bcaa7680c816834fb639
Biplab Behera <biplab@paytmmloyal.com>
2022-03-29 11:46:24 +0530

    manage RBE

---
+++ HPCL.Common/Models/ResponseModel/ManageRbe/RbeUserListResponse.cs
@@ -0,0 +1,22 @@
+﻿using HPCL.Common.Models.ResponseModel.CommonResponse;
+using System.Collections.Generic;
+
+namespace HPCL.Common.Models.ResponseModel.ManageRbe
+{
+    public class RbeUserListResponse : ResponseMsg
+    {
+        public List<RbeUserListResponseData> data { get; set; }
+    }
+
+    public class RbeUserListResponseData
+    {
+        public string RBEID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailId { get; set; }
+        public string Region { get; set; }
+        public string Zone { get; set; }
+        public string ViewKYC { get; set; }
+    }
+}
---
+++ HPCL.Service/Services/ManageRbeService.cs
@@ -0,0 +1,61 @@
+﻿using HPCL.Common.Helper;
+using HPCL.Common.Models.ResponseModel.ManageRbe;
+using HPCL.Common.Models.ViewModel.ManageRbe;
+using HPCL.Service.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Service.Services
+{
+    public class ManageRbeService : IManageRbeService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IRequestService _requestService;
+
+        public ManageRbeService(IHttpContextAccessor httpContextAccessor, IRequestService requestServices)
+        {
+            _httpContextAccessor=httpContextAccessor;
+            _requestService = requestServices;
+        }
+
+        public async Task<ChangeRbeMappingResponse> ChangeRbeMapping(RbeMapping entity)
+        {
+            var reqBody = new RbeMapping
+            {
+                UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
+                UserAgent = CommonBase.useragent,
+                UserIp = CommonBase.userip,
+                FirstName = entity.FirstName,
+                UserName = entity.UserName
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(reqBody), Encoding.UTF8, "application/json");
+            var response = await _requestService.CommonRequestService(content, WebApiUrl.GetMangeRbeMappingUrl);
+            JObject obj = JObject.Parse(JsonConvert.DeserializeObject(response).ToString());
+            ChangeRbeMappingResponse searchList = obj.ToObject<ChangeRbeMappingResponse>();
+            return searchList;
+        }
+
+        public async Task<RbeUserListResponse> RbeUserList(RbeUserList entity)
+        {
+            var reqBody = new RbeUserList
+            {
+                UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
+                UserAgent = CommonBase.useragent,
+                UserIp = CommonBase.userip,
+                FirstName = entity.FirstName,
+                UserName = entity.UserName
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(reqBody), Encoding.UTF8, "application/json");
+            var response = await _requestService.CommonRequestService(content, WebApiUrl.GetRbeUserListUrl);
+            JObject obj = JObject.Parse(JsonConvert.DeserializeObject(response).ToString());
+            RbeUserListResponse searchList = obj.ToObject<RbeUserListResponse>();
+            return searchList;
+        }
+    }
+}
