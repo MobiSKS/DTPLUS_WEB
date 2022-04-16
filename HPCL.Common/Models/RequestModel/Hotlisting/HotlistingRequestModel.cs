2a4b699e0c957295136beec59e6ffc89228467f3
SachithaGirmiti <sachitha@paytmmloyal.com>
2022-04-16 12:52:58 +0530

    Hotlist Approval

--- HPCL.Common/Models/RequestModel/Hotlisting/HotlistingRequestModel.cs
+++ HPCL.Common/Models/RequestModel/Hotlisting/HotlistingRequestModel.cs
@@ -14,5 +14,7 @@ namespace HPCL.Common.Models.RequestModel.Hotlisting
         public int ActionId { get; set; }
         public int ReasonId { get; set; }
         public string ReasonDetails { get; set; }
+        public string FromDate { get; set; }    
+        public string ToDate { get; set; }
     }
 }
--- HPCL.Service/Services/HotlistingService.cs
+++ HPCL.Service/Services/HotlistingService.cs
@@ -17,15 +17,16 @@ using Newtonsoft.Json.Linq;
 
 namespace HPCL.Service.Services
 {
-    public class HotlistingService:IHotlistingService
+    public class HotlistingService : IHotlistingService
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IRequestService _requestService;
-
-        public HotlistingService(IHttpContextAccessor httpContextAccessor, IRequestService requestServices)
+        private readonly ICommonActionService _commonActionService;
+        public HotlistingService(IHttpContextAccessor httpContextAccessor, IRequestService requestServices, ICommonActionService commonActionService)
         {
             _httpContextAccessor = httpContextAccessor;
             _requestService = requestServices;
+            _commonActionService = commonActionService;
         }
         public async Task<string> ApplyHotlistorReactivate([FromBody] HotlistorReactivateViewModel hotlistorReactivateViewModel)
         {
@@ -34,7 +35,7 @@ namespace HPCL.Service.Services
                 UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
                 UserAgent = CommonBase.useragent,
                 UserIp = CommonBase.userip,
-                EntityTypeId = hotlistorReactivateViewModel.EntityTypeId!=""?Convert.ToInt32(hotlistorReactivateViewModel.EntityTypeId):0,
+                EntityTypeId = hotlistorReactivateViewModel.EntityTypeId != "" ? Convert.ToInt32(hotlistorReactivateViewModel.EntityTypeId) : 0,
                 EntityIdVal = hotlistorReactivateViewModel.EntityIdVal,
                 ActionId = hotlistorReactivateViewModel.ActionId != "" ? Convert.ToInt32(hotlistorReactivateViewModel.ActionId) : 0,
                 ReasonId = hotlistorReactivateViewModel.ReasonId != "" ? Convert.ToInt32(hotlistorReactivateViewModel.ReasonId) : 0,
@@ -74,6 +75,58 @@ namespace HPCL.Service.Services
             ViewHotlistingorReactivateResponse = JsonConvert.DeserializeObject<ViewHotlistingorReactivateResponse>(Response);
             return ViewHotlistingorReactivateResponse;
         }
+        public async Task<GetHotlistApprovalResponse> GetHotlistApproval(string EntityTypeId, string ActionId, string FromDate, string ToDate)
+        {
+            string fromDate = "", toDate = "";
 
+            if (!string.IsNullOrEmpty(FromDate) && !string.IsNullOrEmpty(FromDate))
+            {
+                fromDate = await _commonActionService.changeDateFormat(FromDate);
+                toDate = await _commonActionService.changeDateFormat(ToDate);
+            }
+            GetHotlistApprovalResponse getHotlistApprovalResponse = new GetHotlistApprovalResponse();
+            var Request = new HotlistingRequestModel
+            {
+                UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
+                UserAgent = CommonBase.useragent,
+                UserIp = CommonBase.userip,
+                EntityTypeId = Convert.ToInt32(EntityTypeId),
+                ActionId = Convert.ToInt32(ActionId),
+                FromDate = fromDate,
+                ToDate = toDate
+            };
+            StringContent ResponseContent = new StringContent(JsonConvert.SerializeObject(Request), Encoding.UTF8, "application/json");
+            var Response = await _requestService.CommonRequestService(ResponseContent, WebApiUrl.gethotlistapproval);
+            getHotlistApprovalResponse = JsonConvert.DeserializeObject<GetHotlistApprovalResponse>(Response);
+            return getHotlistApprovalResponse;
+        }
+        public async Task<string> UpdateHotlistApproval([FromBody] HotlistApprovalRequest hotlistApprovalRequest)
+        {
+            var hotlistRequest = new HotlistApprovalRequest
+            {
+                UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
+                UserAgent = CommonBase.useragent,
+                UserIp = CommonBase.userip,
+                EntityTypeId =Convert.ToInt32(hotlistApprovalRequest.EntityTypeId) ,
+                ActionId = Convert.ToInt32(hotlistApprovalRequest.ActionId), 
+                ObjUpdateHotlistApprovalEntityCode = hotlistApprovalRequest.ObjUpdateHotlistApprovalEntityCode,
+                ModifiedBy = _httpContextAccessor.HttpContext.Session.GetString("UserId")
+            };
+
+            StringContent requestContent = new StringContent(JsonConvert.SerializeObject(hotlistRequest), Encoding.UTF8, "application/json");
+            var Response = await _requestService.CommonRequestService(requestContent, WebApiUrl.updatehotlistapproval);
+            JObject ResponseObj = JObject.Parse(JsonConvert.DeserializeObject(Response).ToString());
+
+            if (ResponseObj["Status_Code"].ToString() == "200")
+            {
+                var responseJarr = ResponseObj["Data"].Value<JArray>();
+                List<HotlistSuccessResponse> responseList = responseJarr.ToObject<List<HotlistSuccessResponse>>();
+                return responseList.First().Reason.ToString();
+            }
+            else
+            {
+                return ResponseObj["Message"].ToString();
+            }
+        }
     }
 }
