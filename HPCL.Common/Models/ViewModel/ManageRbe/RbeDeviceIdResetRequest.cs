89d77192e930269bcffe40d5bbb129187f01c132
Biplab Behera <biplab@paytmmloyal.com>
2022-04-18 11:36:33 +0530

    fixes

--- HPCL.Common/Models/ViewModel/ManageRbe/RbeDeviceIdResetRequest.cs
+++ HPCL.Common/Models/ViewModel/ManageRbe/RbeDeviceIdResetRequest.cs
@@ -1,10 +1,14 @@
 ﻿using HPCL.Common.Models.CommonEntity;
+using HPCL.Common.Resources;
+using System.ComponentModel.DataAnnotations;
 
 namespace HPCL.Common.Models.ViewModel.ManageRbe
 {
     public class RbeDeviceIdResetRequest : BaseEntity
     {
+        [RegularExpression(FieldValidation.ValidFirstName, ErrorMessage = FieldValidation.ValidFirstNameErrMsg)]
         public string FirstName { get; set; }
+        [RegularExpression(FieldValidation.ValidUserName, ErrorMessage = FieldValidation.ValidUserNameErrMsg)]
         public string MobileNo { get; set; }    
     }
 }
