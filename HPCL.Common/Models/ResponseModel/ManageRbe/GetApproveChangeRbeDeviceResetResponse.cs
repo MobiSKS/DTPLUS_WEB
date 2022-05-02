57861bff0354f9960b271ba635296b59cc2bfd86
Biplab Behera <biplab@paytmmloyal.com>
2022-05-02 11:39:50 +0530

    rbe device id reset req

---
+++ HPCL.Common/Models/ResponseModel/ManageRbe/GetApproveChangeRbeDeviceResetResponse.cs
@@ -0,0 +1,19 @@
+﻿using HPCL.Common.Models.ResponseModel.CommonResponse;
+using System.Collections.Generic;
+
+namespace HPCL.Common.Models.ResponseModel.ManageRbe
+{
+    public class GetApproveChangeRbeDeviceResetResponse : ResponseMsg
+    {
+        public List<GetApproveChangeRbeDeviceResetResponseData> data { get; set; }
+    }
+
+    public class GetApproveChangeRbeDeviceResetResponseData
+    {
+        public string RBEID { get; set; }
+        public string RBEName { get; set; }
+        public string MobileNo { get; set; }
+        public string Region { get; set; }
+        public string RBEDeviceReset { get; set; }
+    }
+}
---
+++ HPCL.Common/Models/ViewModel/ManageRbe/GetApproveChangeRbeDeviceReset.cs
@@ -0,0 +1,19 @@
+﻿using HPCL.Common.Models.CommonEntity;
+using HPCL.Common.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Common.Models.ViewModel.ManageRbe
+{
+    public class GetApproveChangeRbeDeviceReset : BaseEntity
+    {
+        public string ApprovalStatus { get; set; }
+        [RegularExpression(FieldValidation.ValidFirstName, ErrorMessage = FieldValidation.ValidFirstNameErrMsg)]
+        public string FirstName { get; set; }
+        public string RBEId { get; set; }
+    }
+}
