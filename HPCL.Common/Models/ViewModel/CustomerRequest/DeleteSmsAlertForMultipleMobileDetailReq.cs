4b1c3f405a5f5acd693b789e81d5764183d914f6
Biplab Behera <biplab@paytmmloyal.com>
2022-05-19 10:05:33 +0530

    sms alert

---
+++ HPCL.Common/Models/ViewModel/CustomerRequest/DeleteSmsAlertForMultipleMobileDetailReq.cs
@@ -0,0 +1,10 @@
+﻿using HPCL.Common.Models.CommonEntity;
+
+namespace HPCL.Common.Models.ViewModel.CustomerRequest
+{
+    public class DeleteSmsAlertForMultipleMobileDetailReq : BaseEntity
+    {
+        public string CustomerID { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
--- HPCL.Service/Services/CustomerRequestService.cs
+++ HPCL.Service/Services/CustomerRequestService.cs
@@ -1,10 +1,12 @@
 ﻿using HPCL.Common.Helper;
+using HPCL.Common.Models.CommonEntity;
 using HPCL.Common.Models.ResponseModel.CustomerRequest;
 using HPCL.Common.Models.ViewModel.CustomerRequest;
 using HPCL.Service.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,6 +54,45 @@ namespace HPCL.Service.Services
             JObject obj = JObject.Parse(JsonConvert.DeserializeObject(response).ToString());
             GetSmsAlertForMultipleMobileDetailRes searchList = obj.ToObject<GetSmsAlertForMultipleMobileDetailRes>();
             return searchList;
+        }
+
+        public async Task<List<SuccessResponse>> UpdateSmsAlertForMultipleMobileDetail(string customerDetailForSmsAlert)
+        {
+            CustomerDetailForSmsAlert[] arrs = JsonConvert.DeserializeObject<CustomerDetailForSmsAlert[]>(customerDetailForSmsAlert);
+
+            var reqBody = new UpdateSmsAlertForMultipleMobileDetailReq
+            {
+                UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
+                UserAgent = CommonBase.useragent,
+                UserIp = CommonBase.userip,
+                CustomerDetailForSmsAlert = arrs
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(reqBody), Encoding.UTF8, "application/json");
+            var response = await _requestService.CommonRequestService(content, WebApiUrl.UpdateSmsAlertForMultipleMobileDetailUrl);
+            JObject obj = JObject.Parse(JsonConvert.DeserializeObject(response).ToString());
+            var jarr = obj["Data"].Value<JArray>();
+            List<SuccessResponse> reasonList = jarr.ToObject<List<SuccessResponse>>();
+            return reasonList;
+        }
+
+        public async Task<List<SuccessResponse>> DeleteSmsAlertForMultipleMobileDetail(string CustomerID, string MobileNo)
+        {
+            var reqBody = new DeleteSmsAlertForMultipleMobileDetailReq
+            {
+                UserId = _httpContextAccessor.HttpContext.Session.GetString("UserId"),
+                UserAgent = CommonBase.useragent,
+                UserIp = CommonBase.userip,
+                CustomerID = CustomerID,
+                MobileNo = MobileNo
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(reqBody), Encoding.UTF8, "application/json");
+            var response = await _requestService.CommonRequestService(content, WebApiUrl.DeleteSmsAlertForMultipleMobileDetailUrl);
+            JObject obj = JObject.Parse(JsonConvert.DeserializeObject(response).ToString());
+            var jarr = obj["Data"].Value<JArray>();
+            List<SuccessResponse> reasonList = jarr.ToObject<List<SuccessResponse>>();
+            return reasonList;
         }
     }
 }
