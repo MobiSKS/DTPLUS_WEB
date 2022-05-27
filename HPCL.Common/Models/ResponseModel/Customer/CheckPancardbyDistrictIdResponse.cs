f11aba46024b892bc4c8ec2b9dd8f6c396c674f3
ravisamudrala <ravi@paytmmloyal.com>
2022-05-27 13:26:37 +0530

    HDPW-410

---
+++ HPCL.Common/Models/ResponseModel/Customer/CheckPancardbyDistrictIdResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Common.Models.ResponseModel.Customer
+{
+    public class CheckPancardbyDistrictIdResponse
+    {
+        public int Status { get; set; }
+        public string Reason { get; set; }
+        public int Internel_Status_Code { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
