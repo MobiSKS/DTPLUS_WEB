f15770a07dfd2ee4b1515fd51898298887cfa6bf
SachithaGirmiti <sachitha@paytmmloyal.com>
2022-05-27 15:31:37 +0530

    HDPW-407

---
+++ HPCL-Web/Controllers/DealerController.cs
@@ -0,0 +1,91 @@
+﻿using HPCL.Common.Helper;
+using HPCL.Common.Models.RequestModel.Dealer;
+using HPCL.Common.Models.ViewModel.Dealer;
+using HPCL.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Controllers
+{
+    [TypeFilter(typeof(SessionExpireActionFilter))]
+    public class DealerController : Controller
+    {
+
+        private readonly IDealerService _dealerService;
+        private readonly ICommonActionService _commonActionService;
+        public DealerController(IDealerService dealerService, ICommonActionService commonActionService)
+        {
+            _dealerService = dealerService;
+            _commonActionService = commonActionService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+     
+        public async Task<IActionResult> DealerCreditMapping(DealerCreditMappingViewModel entity,string reset,string CustomerID, string success, string error)
+        {
+            if (!string.IsNullOrEmpty(CustomerID))
+                entity.CustomerID = CustomerID;
+            DealerCreditMappingViewModel response = new DealerCreditMappingViewModel();
+            if (entity.CustomerID != null)
+            {
+                response = await _dealerService.GetDealerCreditMappingDetails(entity.CustomerID);
+                response.CustomerID = entity.CustomerID;
+            }
+            ViewBag.Reset = String.IsNullOrEmpty(reset) ? "" : reset;
+            ViewBag.SuccessMessage = success;
+            ViewBag.ErrorMessage = error;
+            ViewBag.CustomerID = String.IsNullOrEmpty(entity.CustomerID)?"": entity.CustomerID;
+            return View(response);
+        }
+ 
+        public IActionResult MapDealerForCreditSale(string CustomerID,string NoofEntries)
+        {
+            ViewBag.CustomerID = CustomerID;
+            DealerForCreditSaleViewModel dealerForCreditSaleViewModel = new DealerForCreditSaleViewModel();
+            dealerForCreditSaleViewModel.CustomerID = CustomerID;
+            dealerForCreditSaleViewModel.NoofEntries = NoofEntries;
+            return View(dealerForCreditSaleViewModel);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveDealerForCreditSale([FromBody] DealerForCreditSaleViewModel dealerForCreditSaleViewModel)
+        {
+            var result = await _dealerService.SaveDealerForCreditSale(dealerForCreditSaleViewModel);
+            return Json(result);
+        }
+        public async Task<IActionResult> MerchantMapEnableorDisable(string customerID, string merchantID, string actionID)
+        {
+            var result = await _dealerService.MerchantMapEnableorDisable(customerID,merchantID, actionID);
+            string succesMsg = "", errorMsg = "";
+            if (result[0].Status == 1)
+                succesMsg = result[0].Reason;
+            else if (result[0].Status == 0)
+                errorMsg = result[0].Reason;
+            return RedirectToAction("DealerCreditMapping", "Dealer", new { CustomerID = customerID , success = succesMsg, error = errorMsg });
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateDealerCreditmapping([FromBody] DealerRequestModel DealerRequestModel)
+        {
+            var result = await _dealerService.UpdateDealerCreditmapping(DealerRequestModel);
+            return Json(result);
+        }
+        public IActionResult DealerCreditPaymentInBulk(string CustomerID)
+        {
+            ViewBag.CustomerID = CustomerID;
+            DealerForCreditSaleViewModel dealerForCreditSaleViewModel = new DealerForCreditSaleViewModel();
+            dealerForCreditSaleViewModel.CustomerID = CustomerID;
+            return View(dealerForCreditSaleViewModel);
+        }
+        public IActionResult UpdateDealerCreditLimit(string CustomerID)
+        {
+            ViewBag.CustomerID = CustomerID;
+            DealerForCreditSaleViewModel dealerForCreditSaleViewModel = new DealerForCreditSaleViewModel();
+            dealerForCreditSaleViewModel.CustomerID = CustomerID;
+            return View(dealerForCreditSaleViewModel);
+        }
+        
+    }
+}
---
+++ HPCL.Common/Models/ViewModel/Dealer/DealerCreditMappingViewModel.cs
@@ -0,0 +1,45 @@
+﻿using HPCL.Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Common.Models.ViewModel.Dealer
+{
+    public class DealerCreditMappingViewModel:CommonResponseBase
+    {
+        public string CustomerID { get; set; }  
+        public GetDealerCreditMappingDetails Data { get; set; }
+        
+    }
+    public class GetDealerCreditMappingDetails
+    {
+        public List<CustomerCCMSBalanceDetails> CustomerCCMSBalanceDetails { get; set; }
+        public List<CustomerDetails> CustomerDetails { get; set; }
+        public List<CustomerMerchantMappedDetails> CustomerMerchantMappedDetails { get; set; }
+    }
+    public class CustomerCCMSBalanceDetails
+    {
+        public string CCMSBalance { get; set; }
+        public string Status { get; set; }
+        public string Reason { get; set; }
+    }
+    public class CustomerDetails
+    {
+        public string IndividualOrgName { get; set; }
+        public string RegionalOfficeName { get; set; }
+        public string ZonalOfficeName { get; set; }
+        public string CustomerAddress { get; set; }
+    }
+    public class CustomerMerchantMappedDetails
+    {
+        public string MerchantId { get; set; }
+        public string OutletNameAndLocation { get; set; }
+        public string CreditCloseLimitType { get; set; }
+        public string CreditCloseLimit { get; set; }
+        public string Outstanding { get; set; }
+        public string CreditCloseLimitBalance { get; set; }
+        public string Status { get; set; }
+    }
+}
