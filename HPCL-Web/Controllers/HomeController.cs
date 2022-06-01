574fc330254c712ed0f20f5116923f21be8ba393
Gaurav Verma <gaurav@paytmmloyal.com>
2022-06-01 16:46:54 +0530

    Commiting Changes for UserRole

--- HPCL-Web/Controllers/HomeController.cs
+++ HPCL-Web/Controllers/HomeController.cs
@@ -123,7 +123,7 @@ namespace HPCL_Web.Controllers
                                         Token = access_token.Result,
                                         UserRole = loginRes[0].UserRole
                                     }
-                                   };
+                                };
 
                                 SessionMenuModel.sessionList.AddRange(sessionData);
                             }
@@ -329,7 +329,7 @@ namespace HPCL_Web.Controllers
         }
 
         [HttpPost]
-        public void SetSessionItems([FromBody] string userId)
+        public async Task<JsonResult> SetSessionItems([FromBody] string userId)
         {
             if (SessionMenuModel.sessionList.Count == 1)
             {
@@ -347,6 +347,8 @@ namespace HPCL_Web.Controllers
                 HttpContext.Session.SetString("BreadCrumbsAction", SessionMenuModel.sessionList[0].BreadCrumbsAction == null ? "" : SessionMenuModel.sessionList[0].BreadCrumbsAction);
                 HttpContext.Session.SetString("CurrentAction", SessionMenuModel.sessionList[0].CurrentAction == null ? "" : SessionMenuModel.sessionList[0].CurrentAction);
                 HttpContext.Session.SetString("BreadCrumbsPerviousMenuName", SessionMenuModel.sessionList[0].BreadCrumbsPerviousMenuName == null ? "" : SessionMenuModel.sessionList[0].BreadCrumbsPerviousMenuName);
+
+                return Json("Success");
             }
             else
             {
@@ -367,6 +369,7 @@ namespace HPCL_Web.Controllers
                     HttpContext.Session.SetString("CurrentAction", item.CurrentAction == null ? "" : item.CurrentAction);
                     HttpContext.Session.SetString("BreadCrumbsPerviousMenuName", item.BreadCrumbsPerviousMenuName == null ? "" : item.BreadCrumbsPerviousMenuName);
                 }
+                return Json("Success");
             }
         }
 
