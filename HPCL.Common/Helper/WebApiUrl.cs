8a56b5c79ffffb8c9ac667c1b171995bd0ad1254
ravisamudrala <ravi@paytmmloyal.com>
2022-06-03 22:25:42 +0530

    HDPW-380

--- HPCL.Common/Helper/WebApiUrl.cs
+++ HPCL.Common/Helper/WebApiUrl.cs
@@ -167,7 +167,7 @@
         public static string getfleetcustomernameandformnumberbyreferenceno = "api/dtplus/aggregatorcustomer/get_aggregator_normal_fleet_customer_name_and_form_number_by_reference_no";
         public static string getunverfiedfleetcustomerbyformnumber = "api/dtplus/aggregatorcustomer/get_unverfied_aggregator_normal_fleet_customer_detail_by_form_number";
         public static string getUpdateContactPersonDetails = "api/dtplus/customer/get_update_contact_person_details";
-        public static string updateContactPersonDetails = "api/dtplus/customer/update_contact_person_details";
+        public static string updateContactPersonDetails = "api/dtplus/customer/update_request_contact_person_details";
         public static string aggregatornormalfleetcustomeraddcard = "api/dtplus/aggregatorcustomer/aggregator_normal_fleet_customer_add_card";
         public static string getCcmsBalAlertConfiguration = "api/dtplus/customer/get_ccms_bal_alert_configuration";
         public static string updateCcmsbalAlertConfiguration = "api/dtplus/customer/update_ccmsbal_alert_configuration";
