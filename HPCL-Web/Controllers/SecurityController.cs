9e232073516e4276f7048ac4b3f0ded21aad9790
ravisamudrala <ravi@paytmmloyal.com>
2022-06-08 14:21:49 +0530

    HDPW-459

--- HPCL-Web/Controllers/SecurityController.cs
+++ HPCL-Web/Controllers/SecurityController.cs
@@ -1,4 +1,5 @@
 ﻿using HPCL.Common.Helper;
+using HPCL.Common.Models.RequestModel.Security;
 using HPCL.Common.Models.ViewModel.Security;
 using HPCL.Service.Interfaces;
 using Microsoft.AspNetCore.Mvc;
@@ -129,6 +130,26 @@ namespace HPCL_Web.Controllers
         public IActionResult AddNewUser()
         {
             return View();
+        }
+        public async Task<IActionResult> UserCreationApprovalNonRBE(UserCreationApprovalNonRBEModel model, string reset, string success, string error, string FirstName, string UserName)
+        {
+            var searchResult = await _securityService.UserCreationApprovalNonRBE(model);
+            ViewBag.Reset = String.IsNullOrEmpty(reset) ? "" : reset;
+            ViewBag.SuccessMessage = success;
+            ViewBag.ErrorMessage = error;
+            if (!String.IsNullOrEmpty(reset))
+            {
+                model.FirstName = "";
+                model.UserName = "";
+            }
+            return View(searchResult);
+        }
+        [HttpPost]
+        public async Task<JsonResult> UserApprovalRejectionNonRBE([FromBody] UserApprovalRejectionRequest model)
+        {
+            var updateKycResponse = await _securityService.UserApprovalRejectionNonRBE(model);
+
+            return Json(new { customer = updateKycResponse });
         }
     }
 }
---
+++ HPCL.Common/Models/RequestModel/Security/UserApprovalRejectionRequest.cs
@@ -0,0 +1,24 @@
+﻿using HPCL.Common.Models.CommonEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Common.Models.RequestModel.Security
+{
+    public class UserApprovalRejectionRequest : BaseEntity
+    {
+        public string ActionType { get; set; }
+        public List<UserApprovalRejectionDetails> TypeApprovalRejectionList { get; set; }
+        public UserApprovalRejectionRequest()
+        {
+            TypeApprovalRejectionList = new List<UserApprovalRejectionDetails>();
+        }
+    }
+    public class UserApprovalRejectionDetails
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+    }
+}
