7219f9e9f87dbb8b5a0aa1e8894419e73a492559
SachithaGirmiti <sachitha@paytmmloyal.com>
2022-06-10 12:53:27 +0530

    HDPW-402,512

---
+++ HPCL.Common/Models/ViewModel/Security/GetUserManageMenuModel.cs
@@ -0,0 +1,27 @@
+﻿using HPCL.Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Common.Models.ViewModel.Security
+{
+    public class GetUserManageMenuModel:CommonResponseBase
+    {
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        public List<GetUserManageMenu> Data { get; set; }
+    }
+    public class GetUserManageMenu
+    {
+        public string MenuId { get; set; }
+        public string MenuName { get; set; }
+        public string ParentMenuId { get; set; }
+        public string MenuLevel { get; set; }
+        public string MenuOrder { get; set; }
+        public string Status { get; set; }
+        public string MenuNameId { get; set; }
+        public string Reason { get; set; }
+    }
+}
