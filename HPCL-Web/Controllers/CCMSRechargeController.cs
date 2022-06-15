1a526f10901f089993c001d77d7c4ed6fd8077e8
Biplab Behera <biplab@paytmmloyal.com>
2022-06-15 18:37:36 +0530

    ccms recharge

---
+++ HPCL-Web/Controllers/CCMSRechargeController.cs
@@ -0,0 +1,35 @@
+﻿using HPCL.Common.Helper;
+using HPCL.Service.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HPCL_Web.Controllers
+{
+    public class CCMSRechargeController : Controller
+    {
+        private readonly ICCMSRechargeService _cCMSRechargeService;
+
+        public CCMSRechargeController(ICCMSRechargeService cCMSRechargeService)
+        {
+            _cCMSRechargeService = cCMSRechargeService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CCMSRechargePage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> CCMSRechargePage(string mobNo)
+        {
+            var searchList = await _cCMSRechargeService.GetDetailsByMObNo(mobNo);
+
+            return Json(new { searchList = searchList });
+        }
+    }
+}
---
+++ HPCL.Service/Interfaces/ICCMSRechargeService.cs
@@ -0,0 +1,12 @@
+﻿using HPCL.Common.Models.ResponseModel.CCMSRecharge;
+using HPCL.Common.Models.ViewModel.CCMSRecharge;
+using System.Threading.Tasks;
+
+namespace HPCL.Service.Interfaces
+{
+    public interface ICCMSRechargeService
+    {
+        Task<GetDetailsByMobRes> GetDetailsByMObNo(string mobNo);
+        Task<RedirectToPGResponse> RedirectToPG(RedirectToPGRequest entity);
+    }
+}
