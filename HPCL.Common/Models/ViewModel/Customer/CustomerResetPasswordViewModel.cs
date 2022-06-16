0bcda6298a08ea54b1b3836e7f4891e8875bf088
SachithaGirmiti <sachitha@paytmmloyal.com>
2022-06-16 16:26:49 +0530

    HDPW-513

--- HPCL.Common/Models/ViewModel/Customer/CustomerResetPasswordViewModel.cs
+++ HPCL.Common/Models/ViewModel/Customer/CustomerResetPasswordViewModel.cs
@@ -11,6 +11,10 @@ namespace HPCL.Common.Models.ViewModel.Customer
     {
         public string CustomerId { get; set; }
         public string AlternateEmailId { get; set; }
+        public List<GetCustomerResetPassword> Data { get; set; }
+    }
+    public class GetCustomerResetPassword
+    {
         public string CommunicationEmailid { get; set; }
         public string Status { get; set; }
         public string Reason { get; set; }
