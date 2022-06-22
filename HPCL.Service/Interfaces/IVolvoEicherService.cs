ce24616b5c415514fb2495348a4755761f502498
SachithaGirmiti <sachitha@paytmmloyal.com>
2022-06-22 09:54:55 +0530

    Volvo Eicher

---
+++ HPCL.Service/Interfaces/IVolvoEicherService.cs
@@ -0,0 +1,16 @@
+﻿using HPCL.Common.Models.ResponseModel.CustomerManage;
+using HPCL.Common.Models.ViewModel.VolvoEicher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPCL.Service.Interfaces
+{
+    public interface IVolvoEicherService
+    {
+        Task<VEDealerOTCCardStatusViewModel> GetVEDealerCardStatus(string DealerCode);
+        Task<List<VECustomerProfileResponse>> BindCustomerDetails(string CustomerId, string NameOnCard);
+    }
+}
