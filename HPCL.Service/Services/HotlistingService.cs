b182d3f2e4a2aa6788666c109d02c2ecd169c6db
Gaurav Verma <gaurav@paytmmloyal.com>
2022-06-27 00:55:29 +0530

    Changes made for Date Show Issue in Hotlist Approval Page

--- HPCL.Service/Services/HotlistingService.cs
+++ HPCL.Service/Services/HotlistingService.cs
@@ -100,18 +100,18 @@ namespace HPCL.Service.Services
             StringContent ResponseContent = new StringContent(JsonConvert.SerializeObject(Request), Encoding.UTF8, "application/json");
             var Response = await _requestService.CommonRequestService(ResponseContent, WebApiUrl.gethotlistapproval);
             getHotlistApprovalResponse = JsonConvert.DeserializeObject<GetHotlistApprovalResponse>(Response);
-            if (getHotlistApprovalResponse.Data != null)
-            {
-                foreach (var item in getHotlistApprovalResponse.Data)
-                {
-                    if (!string.IsNullOrEmpty(item.CreationDate))
-                    {
-                        string[] subs = item.CreationDate.Split('T');
-                        string[] date = subs[0].Split('-');
-                        item.CreationDate= date[1] + "-" + date[2] + "-" + date[0];
-                    }
-                }
-            }
+            //if (getHotlistApprovalResponse.Data != null)
+            //{
+            //    foreach (var item in getHotlistApprovalResponse.Data)
+            //    {
+            //        if (!string.IsNullOrEmpty(item.CreationDate))
+            //        {
+            //            string[] subs = item.CreationDate.Split('T');
+            //            string[] date = subs[0].Split('-');
+            //            item.CreationDate= date[1] + "-" + date[2] + "-" + date[0];
+            //        }
+            //    }
+            //}
             
             return getHotlistApprovalResponse;
         }
