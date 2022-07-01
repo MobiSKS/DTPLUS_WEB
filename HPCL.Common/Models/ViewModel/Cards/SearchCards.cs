855aabf185431f6c52bfbb622dd18f957b9fb273
Biplab Behera <biplab@paytmmloyal.com>
2022-07-01 14:46:40 +0530

    fixes

--- HPCL.Common/Models/ViewModel/Cards/SearchCards.cs
+++ HPCL.Common/Models/ViewModel/Cards/SearchCards.cs
@@ -6,7 +6,7 @@ namespace HPCL.Common.Models.ViewModel.Cards
 {
     public class SearchCards : BaseEntity
     {
-        [Required(ErrorMessage = FieldValidation.CustomerNotEmpty)]
+        //[Required(ErrorMessage = FieldValidation.CustomerNotEmpty)]
         [StringLength(10)]
         [RegularExpression(FieldValidation.ValidCustomerId, ErrorMessage = FieldValidation.ValidCustomerIdErrMsg)]
         public string CustomerId { get; set; }
